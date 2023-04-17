53691410ed1b059a67cf1bdee4a2e5ef895f6133
Phillip Kang <phil@Phillips-MacBook-Pro.local>
2023-04-16 22:41:47 -0700

    module 6 changes

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const controller= require('../controllers/trips');
-router.get('/trips', controller.tripsList);
-router.get('/trips/:tripCode', controller.tripsFindCode);
+const tripsController= require('../controllers/trips');
+
+router
+  .route('/trips')
+  .get(tripsController.tripsList)
+  .post(tripsController.tripsAddTrip);
+
+router
+  .route('/trips/:tripCode')
+  .get(tripsController.tripsFindCode)
+  .put(tripsController.tripsUpdateTrip);
+
 module.exports = router;
